d1c781e54ba7e0579ccd9948bc5d868087584274
BlackBloodLT <lamarks.cavalcanti@gmail.com>
2021-05-06 21:47:37 -0300

    Versão 20210506214700

---
+++ Python3/1_INICIANTE/uri1001.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Extremamente Básico
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A 
+e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo 
+apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo 
+especificado e não esqueça de imprimir o fim de linha após o resultado, caso 
+contrário, você receberá "Presentation Error". 
+Entrada
+A entrada contém 2 valores inteiros. 
+Saída
+Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X 
+e pelo final de linha. Cuide para que tenha um espaço antes e depois do sinal 
+de igualdade, conforme o exemplo abaixo. 
+"""
+
+"""
+Created on Thu May  6 21:39:44 2021
+
+@author: lamarkscavalcanti
+"""
+
+numeroPrimeiro = int(input())
+numeroSegundo = int(input())
+
+soma = numeroPrimeiro+numeroSegundo
+
+print("X =",soma)
